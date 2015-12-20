35c3d174dd76f2e94cfd124d01f8be12bd2e05c1
1m4x <imaxlario@gmail.com>
2015-12-21 00:43:21 +0600

    process management

--- svm/kernel.cpp
+++ svm/kernel.cpp
@@ -43,17 +43,17 @@ namespace svm
         };
         //Process Management
         std::for_each(executables_paths.begin(), executables_paths.end(), [&](const std::string &path) {
-	            CreateProcess(&path);
-	        });
+            CreateProcess(&path);
+        });
 
-          if (!processes.empty()) {
-          			std::cout << "Kernel: set process: " << processes[_current_process_index].id << " for execution." << std::endl;
+        if (!processes.empty()) {
+            std::cout << "Kernel: set process: " << processes[_current_process_index].id << " for execution." << std::endl;
 
-          			board.cpu.registers = processes[_current_process_index].registers;
-          			board.memory.page_table = processes[_current_process_index].page_table;
+            board.cpu.registers = processes[_current_process_index].registers;
+            board.memory.page_table = processes[_current_process_index].page_table;
 
-          			processes[_current_process_index].state = Process::Running;
-          		}
+            processes[_current_process_index].state = Process::Running;
+        }
         if (scheduler == FirstComeFirstServed) {
             board.pic.isr_0 = [&]() {
                 // ToDo: Process the timer interrupt for the FCFS
@@ -61,16 +61,16 @@ namespace svm
 
             board.pic.isr_3 = [&]() {
                 // ToDo: Process the first software interrupt for the FCFS
-          			// Unload the current process
-        				std::cout << "Number of Processes left = " << processes.size() << std::endl;
-        				processes.pop_front();
-        				if (processes.empty()) {
-        					board.Stop();
-        				}
-        				else {
-        					board.cpu.registers = processes[0].registers;
-        					processes[0].state = Process::States::Running;
-        				}
+                // Unload the current process
+                std::cout << "Number of Processes left = " << processes.size() << std::endl;
+                processes.pop_front();
+                if (processes.empty()) {
+                    board.Stop();
+                }
+                else {
+                    board.cpu.registers = processes[0].registers;
+                    processes[0].state = Process::States::Running;
+                }
             };
             } else if (scheduler == ShortestJob) {
             board.pic.isr_0 = [&]() {
@@ -82,17 +82,17 @@ namespace svm
                 // ToDo: Process the first software interrupt for the Shortest
                 //  Job scheduler
                 // Unload the current process
-        				std::cout << "Number of Processes left = " << processes.size() << std::endl;
+                std::cout << "Number of Processes left = " << processes.size() << std::endl;
 
-        				processes.pop_front();
+                processes.pop_front();
 
-        				if (processes.empty()) {
-        					board.Stop();
-        				}
-        				else {
-        					board.cpu.registers = processes[0].registers;
-        					processes[0].state = Process::States::Running;
-        				}
+                if (processes.empty()) {
+                    board.Stop();
+                }
+                else {
+                    board.cpu.registers = processes[0].registers;
+                    processes[0].state = Process::States::Running;
+                }
             };
             } else if (scheduler == RoundRobin) {
             board.pic.isr_0 = [&]() {
@@ -162,7 +162,7 @@ namespace svm
                 std::cout << std::endl;
             };
             } else if (scheduler == Priority) {
-              board.pic.isr_0 = [&]() {
+            board.pic.isr_0 = [&]() {
                 // ToDo: Process the timer interrupt for the Priority Queue
                 //  scheduler
                 ++_cycles_passed_after_preemption;
@@ -189,29 +189,29 @@ namespace svm
             };
 
             board.pic.isr_3 = [&]() {
-              // ToDo: Process the first software interrupt for the Priority
-              //  Queue scheduler
-              // Unload the current process
-              std::cout << "Number of Processes left = " << processes.size() << std::endl;
-              if (board.cpu.registers.a == 1) {
-                priorities.pop();
-                if (priorities.empty()) {
-                  board.Stop();
+                // ToDo: Process the first software interrupt for the Priority
+                //  Queue scheduler
+                // Unload the current process
+                std::cout << "Number of Processes left = " << processes.size() << std::endl;
+                if (board.cpu.registers.a == 1) {
+                    priorities.pop();
+                    if (priorities.empty()) {
+                        board.Stop();
+                    }
+                    else {
+                        Process t = priorities.top();
+                        board.cpu.registers = t.registers;
+                        priorities.pop();
+                        t.state = Process::States::Running;
+                        priorities.push(t);
+                    }
+                    } else if (board.cpu.registers.a == 2) {
+                    Process t = priorities.top();
+                    priorities.pop();
+                    t.priority = board.cpu.registers.b;
+                    t.updateCycles();
+                    priorities.push(t);
                 }
-                else {
-                  Process t = priorities.top();
-                  board.cpu.registers = t.registers;
-                  priorities.pop();
-                  t.state = Process::States::Running;
-                  priorities.push(t);
-                }
-              } else if (board.cpu.registers.a == 2) {
-                Process t = priorities.top();
-                priorities.pop();
-                t.priority = board.cpu.registers.b;
-                t.updateCycles();
-                priorities.push(t);
-              }
             };
         }
 
@@ -226,89 +226,89 @@ namespace svm
 
     void Kernel::CreateProcess(const std::string &name)
     {
-      if (_last_issued_process_id == std::numeric_limits<Process::process_id_type>::max()) {
-        std::cerr << "Kernel: failed to create a new process. The maximum number of processes has been reached." << std::endl;
-      } else {
-        std::ifstream input_stream(name, std::ios::in | std::ios::binary);
-        if (!input_stream) {
-          std::cerr << "Kernel: failed to open the program file." << std::endl;
-        } else {
-          Memory::ram_type ops;
-
-          input_stream.seekg(0, std::ios::end);
-          auto file_size = input_stream.tellg();
-          input_stream.seekg(0, std::ios::beg);
-          ops.resize(static_cast<Memory::ram_size_type>(file_size) / 4);
-
-          input_stream.read(reinterpret_cast<char *>(&ops[0]), file_size);
-
-          if (input_stream.bad()) {
-            std::cerr << "Kernel: failed to read the program file." << std::endl;
-          } else {
-            Memory::ram_size_type new_memory_position = AllocateMemory(ops.size()); // TODO: allocate memory for the process (AllocateMemory)
-            if (new_memory_position == -1) {
-              std::cerr << "Kernel: failed to allocate memory." << std::endl;
+        if (_last_issued_process_id == std::numeric_limits<Process::process_id_type>::max()) {
+            std::cerr << "Kernel: failed to create a new process. The maximum number of processes has been reached." << std::endl;
             } else {
-              std::copy(ops.begin(), ops.end(), (board.memory.ram.begin() + new_memory_position));
+            std::ifstream input_stream(name, std::ios::in | std::ios::binary);
+            if (!input_stream) {
+                std::cerr << "Kernel: failed to open the program file." << std::endl;
+                } else {
+                Memory::ram_type ops;
 
-              Process process(_last_issued_process_id++, new_memory_position,
-                new_memory_position + ops.size());
+                input_stream.seekg(0, std::ios::end);
+                auto file_size = input_stream.tellg();
+                input_stream.seekg(0, std::ios::beg);
+                ops.resize(static_cast<Memory::ram_size_type>(file_size) / 4);
+
+                input_stream.read(reinterpret_cast<char *>(&ops[0]), file_size);
+
+                if (input_stream.bad()) {
+                    std::cerr << "Kernel: failed to read the program file." << std::endl;
+                    } else {
+                    Memory::ram_size_type new_memory_position = AllocateMemory(ops.size()); // TODO: allocate memory for the process (AllocateMemory)
+                    if (new_memory_position == -1) {
+                        std::cerr << "Kernel: failed to allocate memory." << std::endl;
+                        } else {
+                        std::copy(ops.begin(), ops.end(), (board.memory.ram.begin() + new_memory_position));
+
+                        Process process(_last_issued_process_id++, new_memory_position,
+                        new_memory_position + ops.size());
+                    }
+                }
             }
-          }
         }
-      }
     }
 
     Memory::vmem_size_type Kernel::VirtualMemoryToPhysical(Memory::ram_size_type previous_index){
-      Memory::page_index_offset_pair_type page_index_offset = board.memory.GetPageIndexAndOffsetForVirtualAddress(previous_index);
-      Memory::page_entry_type page = board.memory.page_table->at(page_index_offset.first);
+        Memory::page_index_offset_pair_type page_index_offset = board.memory.GetPageIndexAndOffsetForVirtualAddress(previous_index);
+        Memory::page_entry_type page = board.memory.page_table->at(page_index_offset.first);
 
-      Memory::page_entry_type frame = board.memory.AcquireFrame();
+        Memory::page_entry_type frame = board.memory.AcquireFrame();
 
-      if (page == Memory::INVALID_PAGE){
-        processes[_current_process_index].page_table->at(previous_index) = frame;
-        return frame + page_index_offset.second;
-      }
-      else
-      {
-        board.Stop();
-      }
+        if (page == Memory::INVALID_PAGE){
+            processes[_current_process_index].page_table->at(previous_index) = frame;
+            return frame + page_index_offset.second;
+        }
+        else
+        {
+            board.Stop();
+        }
     }
 
     Memory::ram_size_type Kernel::AllocateMemory(Memory::ram_size_type units)
     {
-      Memory::ram_size_type prev_index = _free_physical_memory_index;
-      Memory::ram_size_type current_index;
+        Memory::ram_size_type prev_index = _free_physical_memory_index;
+        Memory::ram_size_type current_index;
 
 
-      for(Memory::ram_size_type next_free_index = board.memory.ram[VirtualMemoryToPhysical(prev_index)]; ;  prev_index = next_free_index, next_free_index = board.memory.ram[next_free_index])
-      {
-        Memory::ram_size_type size = board.memory.ram[VirtualMemoryToPhysical(next_free_index+1)];
-        if( size >= units)
+        for(Memory::ram_size_type next_free_index = board.memory.ram[VirtualMemoryToPhysical(prev_index)]; ;  prev_index = next_free_index, next_free_index = board.memory.ram[next_free_index])
         {
-          if(size == units)
-          {
-            board.memory.ram[VirtualMemoryToPhysical(prev_index)] = board.memory.ram[VirtualMemoryToPhysical(next_free_index)];
-          }
-          else
-          {
-            board.memory.ram[VirtualMemoryToPhysical(next_free_index + 1)] -= units + 2;
-            next_free_index +=  board.memory.ram[VirtualMemoryToPhysical(next_free_index + 1)];
+            Memory::ram_size_type size = board.memory.ram[VirtualMemoryToPhysical(next_free_index+1)];
+            if( size >= units)
+            {
+                if(size == units)
+                {
+                    board.memory.ram[VirtualMemoryToPhysical(prev_index)] = board.memory.ram[VirtualMemoryToPhysical(next_free_index)];
+                }
+                else
+                {
+                    board.memory.ram[VirtualMemoryToPhysical(next_free_index + 1)] -= units + 2;
+                    next_free_index +=  board.memory.ram[VirtualMemoryToPhysical(next_free_index + 1)];
 
-            board.memory.ram[VirtualMemoryToPhysical(next_free_index + 1)] = units;
-          }
-          _free_physical_memory_index = prev_index;
+                    board.memory.ram[VirtualMemoryToPhysical(next_free_index + 1)] = units;
+                }
+                _free_physical_memory_index = prev_index;
 
-          return next_free_index + 2;
+                return next_free_index + 2;
 
+            }
+            if(next_free_index == _free_physical_memory_index)
+            {
+                return NULL;
+            }
         }
-        if(next_free_index == _free_physical_memory_index)
-        {
-          return NULL;
-        }
-      }
 
-      return -1;
+        return -1;
     }
 
     void Kernel::FreeMemory(Memory::ram_size_type physical_memory_index)
